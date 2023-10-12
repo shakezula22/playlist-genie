e120d9e2a1229cfb08a22792505ea41d5c02ef0b
Diana Gude <dianaml05@gmail.com>
2023-10-12 11:05:27 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main>
+      <div>
+        <div className="card">
+          <h1 className="card__title">
+            Playlist<br></br> Genie
+          </h1>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
